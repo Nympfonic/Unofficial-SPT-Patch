4226f6114bc7b678f83891c25604cca85ed1fd8d
Nympfonic <arys.steam@gmail.com>
2024-02-17 03:35:02 +0000

    Initial plugin creation

---
+++ SPTBugfixes/Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace SPTBugfixes
+{
+    [BepInPlugin("com.Arys.SPTBugfixes", "Arys' SPT Bugfixes", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
